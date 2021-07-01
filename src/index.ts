78e69eca20c467c9b88b9db9497e50b3ec9c041d
myoshimi <443030net@gmail.com>
2021-07-01 10:51:12 +0900

    WebSocketサーバのテスト

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+import websocket from 'ws'
+import http from 'http'
+// yarn add -D ws @types/ws
 
-console.log(hello("TypeScript"));
+// WebSocketサーバ
+// UnixドメインソケットでWebSocketを待ち受けるHTTPサーバ
+// const hs: http.Server = http.createServer();
+// const wss = new websocket.Server({server: hs})
+// HTTPサーバのListen開始
+// hs.listen(socketPath, () => {
+//   console.log(`websocket server listening on ${socketPath}`);
+// })
+const wss = new websocket.Server({port:8000});
+
+wss.on('connection', ws => {
+  ws.send('hello, this is websocket server.');
+  ws.on('message', message => {
+    console.log(message);
+    ws.send(message);
+  })
+  ws.on('close', () => {
+    console.log('lost a client.');
+  } )
+})
 
